7452bb08b066dc940607bed398d82e3834832f57
Roi Avinooam <avinoamr@gmail.com>
2019-08-30 14:45:00 -0700

    Consolidate the example in one file to make it clearer how the viewport should be configured

--- example/js/main.js
+++ example/js/main.js
@@ -1,65 +1,65 @@
 (function() {
     var websheet = window.websheet()
-        // .column( "name" )
-        // .column( "B" )
-        // .column( "C" )
-        // .column( "D" )
-        // .column( "E" )
-        // .column( "F" )
-        // .column( "G" )
-        // .column( "H" )
-        // .column( "I" )
+        .column( "name" )
+        .column( "B" )
+        .column( "C" )
+        .column( "D" )
+        .column( "E" )
+        .column( "F" )
+        .column( "G" )
+        .column( "H" )
+        .column( "I" )
 
     d3.select( "#sheet" )
         .datum([
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
-            // { "name": "Alice", "B": 100 },
-            // { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
+            { "name": "Alice", "B": 100 },
+            { "name": "Bob", "B": 200 },
         ])
         .call( websheet )
 
